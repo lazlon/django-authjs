60f358efb830dc829fa410073a252f42a07b8f1d
Aylur <k.demeter@protonmail.com>
2024-08-24 12:24:44 +0200

    remove tokens from admin

--- authjs/admin.py
+++ authjs/admin.py
@@ -1,10 +1,10 @@
-import django.contrib.sessions.models as s
+# import django.contrib.sessions.models as s
 from django.contrib import admin
 
 import authjs.models as m
 
 admin.site.register(m.User)
 admin.site.register(m.Account)
-admin.site.register(m.Session)
-admin.site.register(m.VerificationToken)
-admin.site.register(s.Session)
+# admin.site.register(m.Session)
+# admin.site.register(m.VerificationToken)
+# admin.site.register(s.Session)
